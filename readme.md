d753d92198c585d236095c56c2312ae9889ec5ce
Mark Wetter <code@markwetter.com>
2018-06-13 16:14:29 -0700

    Initial Commit

---
+++ readme.md
@@ -0,0 +1,53 @@
+# mc-wrap
+
+A lightweight wrapper for Minecraft Server running in Docker. Intercepts SIGINT signals from the environment and sends a graceful shutdown command to the child server process.
+
+## Usage
+
+Invoke mc-wrap before your normal Minecraft Server startup command.
+
+```
+mc-wrap java -jar server.jar
+```
+You can still pass JVM arguments.
+```
+mc-wrap java \
+    -Xms2G -Xmx2G \
+    -XX:+UseG1GC \
+    -XX:+UnlockExperimentalVMOptions \
+    -XX:MaxGCPauseMillis=100 \
+    -XX:+DisableExplicitGC \
+    -XX:TargetSurvivorRatio=90 \
+    -XX:G1NewSizePercent=50 \
+    -XX:G1MaxNewSizePercent=80 \
+    -XX:InitiatingHeapOccupancyPercent=10 \
+    -XX:G1MixedGCLiveThresholdPercent=50 \
+    -XX:+AggressiveOpts \
+    -XX:+AlwaysPreTouch \
+    -jar server.jar
+```
+
+## Compiling from source
+
+You will need the Rust language build tools.
+
+https://www.rust-lang.org/en-US/install.html
+
+```
+git clone git@gitlab.com:markwetter/mc-wrap.git
+cd mc-wrap
+cargo build --release
+```
+
+## Authors
+* **Mark Wetter** - [Gitlab](https://gitlab.com/markwetter)
+
+## Acknowledgments
+
+* **Geoff Bourne** - *I was inspired by Geoff's mc-server-runner that I found while researching my problem with shutting down Minecraft gracefully from inside a docker container* [Github](https://github.com/itzg/mc-server-runner)
+
+## License
+
+mc-wrap is distributed under the terms of the MIT license
+
+See LICENSE for details.
---
+++ src/main.rs
@@ -0,0 +1,54 @@
+#[macro_use]
+extern crate chan;
+extern crate chan_signal;
+extern crate os_pipe;
+
+use std::io::Write;
+use std::process::Command;
+use std::process::Stdio;
+use std::collections::VecDeque;
+use std::thread;
+use std::env;
+
+use chan_signal::Signal;
+use os_pipe::{pipe, IntoStdio};
+
+fn main() {
+    if env::args().count() < 2 {
+        eprintln!("At least one argument required (try ./mc-oxidize java -jar server.jar)");
+        std::process::exit(1);
+    }
+
+    let signal = chan_signal::notify(&[Signal::INT, Signal::TERM]);
+    let (sdone, rdone) = chan::sync(0);
+    let (stdout, mut stdin) = pipe().unwrap();
+
+    thread::spawn(move || run(sdone, stdout));
+
+    loop {
+        chan_select! {
+            signal.recv() -> _signal => {
+                println!("Shutting down Minecraft server...");
+                stdin.write(b"stop\n").unwrap();
+            },
+            rdone.recv() => {
+                println!("Detected Minecraft server shutdown");
+                break;
+            }
+        }
+    }
+}
+
+fn run(_sdone: chan::Sender<()>, stdout: os_pipe::PipeReader) {
+    let mut args: VecDeque<String> = env::args().collect();
+    args.pop_front();
+
+    Command::new(args.pop_front().unwrap())
+        .args(args)
+        .stdin(stdout.into_stdio())
+        .stdout(Stdio::inherit())
+        .spawn()
+        .expect("Failed to spawn child")
+        .wait()
+        .expect("Failed to wait on child");
+}
